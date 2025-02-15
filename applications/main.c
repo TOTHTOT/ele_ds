9f9d33429eea24999044b3f6b3b0d606356d9f64
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2025-02-15 12:33:14 +0800

    Initial commit

---
+++ applications/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-27     balanceTWK   first version
+ * 2023-12-03     Meco Man     support nano version
+ */
+
+#include <board.h>
+#include <rtthread.h>
+#include <drv_gpio.h>
+#ifndef RT_USING_NANO
+#include <rtdevice.h>
+#endif /* RT_USING_NANO */
+
+/* defined the LED0 pin: PC0 */
+#define LED0_PIN    GET_PIN(C, 13)
+
+int main(void)
+{
+    /* set LED0 pin mode to output */
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+
+    while (1)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+}
+static int ota_app_vtor_reconfig(void)
+{
+ #define NVIC_VTOR_MASK 0x3FFFFF80
+ #define RT_APP_PART_ADDR 0x08008000
+ /* ????????? */
+ SCB->VTOR = RT_APP_PART_ADDR & NVIC_VTOR_MASK;
+
+ return 0;
+}
+INIT_BOARD_EXPORT(ota_app_vtor_reconfig);
