5855fbecfb5df23b2f7aac6e6b777cb60d1b0dc6
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2025-04-08 17:54:22 +0800

    关闭日志

--- applications/lvgl/lv_conf.h
+++ applications/lvgl/lv_conf.h
@@ -1,4 +1,12 @@
 /*
+ * @Author: TOTHTOT 37585883+TOTHTOT@users.noreply.github.com
+ * @Date: 2025-04-07 14:15:53
+ * @LastEditors: TOTHTOT 37585883+TOTHTOT@users.noreply.github.com
+ * @LastEditTime: 2025-04-08 17:40:09
+ * @FilePath: \ele_ds\applications\lvgl\lv_conf.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
  * Copyright (c) 2006-2021, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -16,6 +24,8 @@
 #define MY_DISP_HOR_RES          176
 #define MY_DISP_VER_RES          264
 #define LV_COLOR_DEPTH 1       // 1-bit 色深，适合墨水屏
+#define LV_USE_BTN        1
+
 // #define LV_USE_PERF_MONITOR 0
 #define LV_COLOR_16_SWAP 0
 // #define LV_USE_LOG 1
